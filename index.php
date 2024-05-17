28be7f786dbe6d8a3cbb4b9a5e2aa64a5066d2a6
Fuck it <vincentklein57950@outlook.fr>
2024-05-17 14:48:35 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="site d'un artiste rap français de Metz">
+    <title>Joker IBRAH</title>
+    <style></style>
+    
+</head>
+<body>
+<h1>JokerIBRAH</h1>
+<h2>Histoire</h2>
+<p>
+
+</p>
+<h3>Theme utilisé</h3>
+<p>
+
+</p>
+<h4>Ou peut on l'écouter?</h4>
+<p>
+
+</p>
+<footer>
+    <p><a href=#>Contact</a> </p>
+</footer>
+
+</body>
+
+
+
+
+
+
+
+
+</html>
