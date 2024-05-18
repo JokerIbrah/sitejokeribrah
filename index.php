5f947ce5845bf099cbaf468a4dd5f6472ea165b3
Fuck it <vincentklein57950@outlook.fr>
2024-05-18 16:47:36 +0200

    addfiles

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="site d'un artiste rap français de Metz">
     <title>Joker IBRAH</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Lien vers le fichier CSS -->
+    <link rel="stylesheet" href="style.css"> <!-- Lien vers le fichier CSS -->
     
 </head>
 <body>
---
+++ page histoire.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="site d'un artiste rap français de Metz">
+    <title>Joker IBRAH Histoire</title>
+    <link rel="stylesheet" href="style.css"> <!-- Lien vers le fichier CSS -->
+    
+</head>
+<!-- On va mettre le logo animé de Joker IBRAH-->
+<h1>JokerIBRAH</h1>
+<h2>Histoire</h2>
+<p>
+   Originaire d'une ville du grand-est de la France, il a beaucoup voyagé. Empreint de désire de changer durablement la société il s'est engagé. Ces texte sont sa liberté d'expression. Il pratique un genre de punk-rap appelé le rapn'roll, ne suivant pas les règles prédéfini du rap. Il va au dessus de cela.
+</p>
+<footer>
+    <p><a href=#>Contact</a> </p>
+</footer>
