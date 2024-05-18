f8f7c3a5c5c8e43a434d7f93ea7a4bfb64ce1f3e
Fuck it <vincentklein57950@outlook.fr>
2024-05-18 16:58:15 +0200

    addfiles

---
+++ page ou l'écouté.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="site d'un artiste rap français de Metz">
+    <title>Joker IBRAH</title>
+    <link rel="stylesheet" href="style.css"> <!-- Lien vers le fichier CSS -->
+    
+</head>
+<h4>Ou peut on l'écouter?</h4>
+            Sur les plateformes de streaming: 
+                <ul>
+                    <div class="linkstream">
+                    <li><img src="logo deezer.jpeg" alt="logo deezer">
+                     <a href="https://www.deezer.com/fr/artist/180073417?utm_campaign=clipboard-generic&utm_source=user_sharing&utm_medium=mobile&utm_content=artist-180073417&deferredFl=1">DEEZER</a> </li>
+                    <li>
+                    <img src="logo spotify.jpg" alt="logo spotify">
+                    <a href="https://open.spotify.com/intl-fr/artist/17uEp1U99X0z16038VvmqF?utm_medium=share&utm_source=linktree">SPOTIFY</a></li>
+                    <li>
+                    <img src="logo bandcamp.jpeg" alt="logo Bandcamp">
+                        <a href="https://jokeribrah.bandcamp.com/album/acab-all-clitoris-are-beautifull">BANDCAMP</a></li>
+                    <li>
+                    <img src="logo youtube.png" alt="logo youtube">
+                    <a href="https://www.youtube.com/channel/UCRion2_smYWR4dREoRZF94A">YOUTUBE</a></li>
+                    </div>
+                </ul>
+<p>
+
+</p>
+<footer>
+    <p><a href=#>Contact</a> </p>
+</footer>
