c36db200c911f5216368e219ef8ee20c142d2638
Fuck it <vincentklein57950@outlook.fr>
2024-05-18 16:34:51 +0200

    addfiles

--- index.php
+++ index.php
@@ -5,20 +5,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="site d'un artiste rap français de Metz">
     <title>Joker IBRAH</title>
-    <style></style>
+    <link rel="stylesheet" href="styles.css"> <!-- Lien vers le fichier CSS -->
     
 </head>
 <body>
+    <!-- On va mettre le logo animé de Joker IBRAH-->
 <h1>JokerIBRAH</h1>
 <h2>Histoire</h2>
 <p>
-
+   Originaire d'une ville du grand-est de la France, il a beaucoup voyagé. Empreint de désire de changer durablement la société il s'est engagé. Ces texte sont sa liberté d'expression. Il pratique un genre de punk-rap appelé le rapn'roll, ne suivant pas les règles prédéfini du rap. Il va au dessus de cela.
 </p>
 <h3>Theme utilisé</h3>
-<p>
+<p>Les principaux thèmes qu'il utilise:</p>
+<ul>
+    <li>anti- capitalisme</li>
+    <li>haine envers la société</li>
+    <li>sa vie personnelle</li>
+    <li>ego-trip</li>
+</ul>
 
-</p>
 <h4>Ou peut on l'écouter?</h4>
+            Sur les plateformes de streaming: 
+                <ul>
+                    <div class="linkstream">
+                    <li><img src="logo deezer.jpeg" alt="logo deezer">
+                     <a href="https://www.deezer.com/fr/artist/180073417?utm_campaign=clipboard-generic&utm_source=user_sharing&utm_medium=mobile&utm_content=artist-180073417&deferredFl=1">DEEZER</a> </li>
+                    <li>
+                    <img src="logo spotify.jpg" alt="logo spotify">
+                    <a href="https://open.spotify.com/intl-fr/artist/17uEp1U99X0z16038VvmqF?utm_medium=share&utm_source=linktree">SPOTIFY</a></li>
+                    <li>
+                    <img src="logo bandcamp.jpeg" alt="logo Bandcamp">
+                        <a href="https://jokeribrah.bandcamp.com/album/acab-all-clitoris-are-beautifull">BANDCAMP</a></li>
+                    <li>
+                    <img src="logo youtube.png" alt="logo youtube">
+                    <a href="https://www.youtube.com/channel/UCRion2_smYWR4dREoRZF94A">YOUTUBE</a></li>
+                    </div>
+                </ul>
 <p>
 
 </p>
