f055fc0bcfdfe40ed6f716c9906ee28c09ca8c7d
Fuck it <vincentklein57950@outlook.fr>
2024-05-18 16:57:34 +0200

    addfiles

--- index.php
+++ index.php
@@ -16,29 +16,7 @@
    Originaire d'une ville du grand-est de la France, il a beaucoup voyagé. Empreint de désire de changer durablement la société il s'est engagé. Ces texte sont sa liberté d'expression. Il pratique un genre de punk-rap appelé le rapn'roll, ne suivant pas les règles prédéfini du rap. Il va au dessus de cela.
 </p>
 
-<h4>Ou peut on l'écouter?</h4>
-            Sur les plateformes de streaming: 
-                <ul>
-                    <div class="linkstream">
-                    <li><img src="logo deezer.jpeg" alt="logo deezer">
-                     <a href="https://www.deezer.com/fr/artist/180073417?utm_campaign=clipboard-generic&utm_source=user_sharing&utm_medium=mobile&utm_content=artist-180073417&deferredFl=1">DEEZER</a> </li>
-                    <li>
-                    <img src="logo spotify.jpg" alt="logo spotify">
-                    <a href="https://open.spotify.com/intl-fr/artist/17uEp1U99X0z16038VvmqF?utm_medium=share&utm_source=linktree">SPOTIFY</a></li>
-                    <li>
-                    <img src="logo bandcamp.jpeg" alt="logo Bandcamp">
-                        <a href="https://jokeribrah.bandcamp.com/album/acab-all-clitoris-are-beautifull">BANDCAMP</a></li>
-                    <li>
-                    <img src="logo youtube.png" alt="logo youtube">
-                    <a href="https://www.youtube.com/channel/UCRion2_smYWR4dREoRZF94A">YOUTUBE</a></li>
-                    </div>
-                </ul>
-<p>
 
-</p>
-<footer>
-    <p><a href=#>Contact</a> </p>
-</footer>
 
 </body>
 
